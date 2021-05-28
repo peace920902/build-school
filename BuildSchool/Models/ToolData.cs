82738fadd836c44ca5237cebc7c8874f34d1e72d
peace920902 <peace9209021@gmail.com>
2021-05-28 21:27:48 +0800

    tools

---
+++ BuildSchool/Models/ToolData.cs
@@ -0,0 +1,10 @@
+﻿namespace BuildSchool.Models
+{
+    public class ToolData
+    {
+        public string MainImgPath { get; set; }
+        public string SubImgPath { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
